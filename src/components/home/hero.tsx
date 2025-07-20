28e996b6bd8fd01009478c344ac3fd998a89e12c
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2025-07-20 22:31:09 +0300

    fixes

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -4,16 +4,16 @@ import { LavaLamp } from '../ui/fluid-blob'
 export function Hero() {
 	return (
 		<section className='w-full min-h-screen flex flex-col justify-center items-center relative gap-8 px-3'>
-			<h1 className='text-6xl font-black text-center max-lg:text-5xl max-sm:text-3xl mix-blend-difference text-white'>
+			<h1 className='text-6xl font-black text-center max-lg:text-5xl max-sm:text-3xl mix-blend-exclusion text-white will-change-transform'>
 				Керуй командою легко.
 			</h1>
-			<p className='text-lg font-medium text-center max-w-2/3 max-sm:max-w-full max-sm:text-base mix-blend-difference text-white'>
+			<p className='text-lg font-medium text-center max-w-2/3 max-sm:max-w-full max-sm:text-base mix-blend-exclusion text-white will-change-transform'>
 				Все необхідне для управління проєктами в одному зрозумілому інтерфейсі.
 				Для команд будь-якого розміру й досвіду.
 			</p>
 			<div className='flex flex-col items-center justify-center gap-2'>
 				<Button variant='primary'>Почати, це безкоштовно!</Button>
-				<h2 className='text-xs font-medium text-center mix-blend-difference text-white'>
+				<h2 className='text-xs font-medium text-center mix-blend-exclusion text-white will-change-transform'>
 					Налаштування роботи всього за 5 хвилин
 				</h2>
 			</div>
